22c3051ac455ad41b5d6c37309ad60e6ef69daa7
hazemelwaleed45 <hazemelwaleed45@gmail.com>
2024-08-18 04:41:21 +0300

    Fix Some bags

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
-use App\Http\Controllers\CourseController;
-use App\Http\Controllers\TrackController;
+
+use App\Http\Controllers\Api\TrackController;
+use App\Http\Controllers\Api\CourseController;
+// use App\Http\Controllers\CourseController;
+// use App\Http\Controllers\TrackController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
